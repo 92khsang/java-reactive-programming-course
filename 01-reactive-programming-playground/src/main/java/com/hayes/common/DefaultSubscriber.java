66f05998eae54da5bfad80ddc7dfc3c3dfa6bcd2
Hayes <92khsang@gmail.com>
2025-06-18 17:10:31 +0900

    Lecture 19. Creating Default Subscriber

---
+++ 01-reactive-programming-playground/src/main/java/com/hayes/common/DefaultSubscriber.java
@@ -0,0 +1,40 @@
+package com.hayes.common;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DefaultSubscriber<T> implements Subscriber<T> {
+
+	private static final Logger log = LoggerFactory.getLogger(DefaultSubscriber.class);
+	private final String name;
+
+	public DefaultSubscriber(String name) {
+		this.name = name != null ? name : "";
+	}
+
+	@Override
+	public void onSubscribe(Subscription subscription) {
+		subscription.request(Long.MAX_VALUE);
+	}
+
+	@Override
+	public void onNext(T item) {
+		log.info("{}received: {}", prefix(), item);
+	}
+
+	@Override
+	public void onError(Throwable throwable) {
+		log.error("{}error", prefix(), throwable);
+	}
+
+	@Override
+	public void onComplete() {
+		log.info("{}completed!", prefix());
+	}
+
+	private String prefix() {
+		return name.isBlank() ? "" : "[" + name + "] ";
+	}
+}
