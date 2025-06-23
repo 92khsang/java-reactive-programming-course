be64a75518fa846dca764d2cc4a30f8267dba27c
Hayes <92khsang@gmail.com>
2025-06-23 11:37:37 +0900

    Lecture 80. Error Handling - Part 2

--- 01-reactive-programming-playground/src/main/java/com/hayes/sec05/Lec06ErrorHandling.java
+++ 01-reactive-programming-playground/src/main/java/com/hayes/sec05/Lec06ErrorHandling.java
@@ -10,7 +10,8 @@ import reactor.core.publisher.Mono;
 public class Lec06ErrorHandling {
 
 	public static void main(String[] args) {
-		onErrorReturnExample();
+//		onErrorReturnExample();
+		onErrorResumeExample();
 	}
 
 	private static void onErrorReturnExample() {
@@ -40,10 +41,53 @@ public class Lec06ErrorHandling {
 				.subscribe(Util.subscriber("Demo01"));
 	}
 
+	private static void onErrorResumeExample() {
+		Mono.just(5)
+				.map(Lec06ErrorHandling::processItem)
+				.map(String::valueOf)
+				// Order matters: match from top to bottom
+				.onErrorResume(ArithmeticException.class, e -> fallback(ArithmeticException.class.getName()))
+				.onErrorResume(Exception.class, e -> fallback(Exception.class.getName()))
+				.subscribe(Util.subscriber("Demo02"));
+
+		Mono.just(5)
+				.map(Lec06ErrorHandling::processItem)
+				.map(String::valueOf)
+				.onErrorResume(Exception.class, e -> fallback(Exception.class.getName()))
+				.onErrorResume(ArithmeticException.class, e -> fallback(ArithmeticException.class.getName()))
+				.subscribe(Util.subscriber("Demo02"));
+
+		Mono.just(5)
+				.map(Lec06ErrorHandling::processItem)
+				.map(String::valueOf)
+				.onErrorResume(IllegalArgumentException.class, e -> fallback(IllegalArgumentException.class.getName()))
+				.onErrorResume(e -> fallback(Exception.class.getName()))
+				.onErrorReturn("Unknown")
+				.subscribe(Util.subscriber("Demo02"));
+
+		Mono.just(5)
+				.map(Lec06ErrorHandling::processItem)
+				.map(String::valueOf)
+				.onErrorResume(e -> fallbackWithError())
+				.onErrorReturn("Fail In Fallback")
+				.subscribe(Util.subscriber("Demo02"));
+	}
+
 	private static int processItem(int i) {
 		if (i == 5) {
 			return i / 0; // Triggers ArithmeticException
 		}
 		return i;
 	}
+
+	private static Mono<String> fallback(String exceptionName) {
+		return Mono.fromSupplier(() -> exceptionName);
+	}
+
+	private static Mono<String> fallbackWithError() {
+		return Mono.fromSupplier(() -> {
+			throw new RuntimeException("RuntimeException");
+		});
+	}
+
 }
