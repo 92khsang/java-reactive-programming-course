1dab4cae021065bf8cb2e1ea4b5d5fd6a92cb0bc
Hayes <92khsang@gmail.com>
2025-06-25 15:43:40 +0900

    Lecture 140. Flux - flatMap

---
+++ 01-reactive-programming-playground/src/main/java/com/hayes/sec09/Lec11FluxFlatMap.java
@@ -0,0 +1,29 @@
+package com.hayes.sec09;
+
+import com.hayes.common.Util;
+import com.hayes.sec09.applications.Order;
+import com.hayes.sec09.applications.OrderService;
+import com.hayes.sec09.applications.User;
+import com.hayes.sec09.applications.UserService;
+import reactor.core.publisher.Flux;
+
+/*
+    Sequential non-blocking IO calls!
+    flatMap is used to flatten the inner publisher / to subscribe to the inner publisher
+ */
+public class Lec11FluxFlatMap {
+
+	public static void main(String[] args) {
+
+		Flux<User> users = UserService.getAllUsers();
+		Flux<Order> orders = users
+				.map(User::id)
+				.flatMap(OrderService::getUserOrders);
+
+		orders
+				.subscribe(Util.subscriber("Flux2Flux"));
+
+		Util.sleepSeconds(2);
+	}
+
+}
