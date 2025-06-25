718cf05a045f1114d5bb3318ab5da10ed98415d5
Hayes <92khsang@gmail.com>
2025-06-25 09:24:22 +0900

    Lecture 122. Error Strategy

--- 01-reactive-programming-playground/src/main/java/com/hayes/sec08/Lec05BackPressureStrategies.java
+++ 01-reactive-programming-playground/src/main/java/com/hayes/sec08/Lec05BackPressureStrategies.java
@@ -1,5 +1,7 @@
 package com.hayes.sec08;
 
+import java.util.function.UnaryOperator;
+
 import com.hayes.common.Util;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
@@ -16,7 +18,8 @@ import reactor.core.scheduler.Schedulers;
 public class Lec05BackPressureStrategies {
 
 	public static void main(String[] args) {
-		bufferStrategy();
+//		bufferStrategy();
+		errorStrategy();
 	}
 
 	private static void bufferStrategy() {
@@ -24,15 +27,26 @@ public class Lec05BackPressureStrategies {
 
 		producer
 				.onBackpressureBuffer()
-				.limitRate(1)
-				.publishOn(Schedulers.boundedElastic())
-				.map(Lec05BackPressureStrategies::timeConsumingTask)
+				.transform(appendSuffix())
 				.subscribe(Util.subscriber("BufferStrategy"));
 
 		Util.sleepSeconds(10);
 	}
 
-	public static Flux<Integer> createFlux() {
+	private static void errorStrategy() {
+		Flux<Integer> producer = createFlux();
+
+		producer
+				.log("Tag 1")
+				.onBackpressureError()
+				.log("Tag 2")
+				.transform(appendSuffix())
+				.subscribe(Util.subscriber("ErrorStrategy"));
+
+		Util.sleepSeconds(10);
+	}
+
+	private static Flux<Integer> createFlux() {
 		return Flux.create(sink -> {
 					for (int i = 1; i <= 100 && !sink.isCancelled(); i++) {
 						log.info("Generating {}", i);
@@ -43,6 +57,13 @@ public class Lec05BackPressureStrategies {
 				})
 				.cast(Integer.class)
 				.subscribeOn(Schedulers.parallel());
+	}
+
+	private static UnaryOperator<Flux<Integer>> appendSuffix() {
+		return flux -> flux
+				.limitRate(1)
+				.publishOn(Schedulers.boundedElastic())
+				.map(Lec05BackPressureStrategies::timeConsumingTask);
 	}
 
 
