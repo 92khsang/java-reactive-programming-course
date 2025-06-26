b160b0070cef2cbb3a8dfef2293efcb575474c51
Hayes <92khsang@gmail.com>
2025-06-26 10:01:23 +0900

    Lecture 162. Repeat - Part 2

--- 01-reactive-programming-playground/src/main/java/com/hayes/sec11/Lec01Repeat.java
+++ 01-reactive-programming-playground/src/main/java/com/hayes/sec11/Lec01Repeat.java
@@ -1,6 +1,10 @@
 package com.hayes.sec11;
 
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import com.hayes.common.Util;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /*
@@ -10,18 +14,62 @@ import reactor.core.publisher.Mono;
 public class Lec01Repeat {
 
 	public static void main(String[] args) {
-		repeatCountry(0);
-		Util.printCutoffLIne();
-		repeatCountry(5);
+//		repeatCountry(0);
+//		repeatCountry(5);
+//		conditionalRepeat();
+//		conditionAsBooleanSupplier();
+//		delayedRepeat();
+//		delayedRepeatWithTake();
+		repeatWithFlux();
 	}
 
 	private static void repeatCountry(Integer repeatCount) {
-		Mono<String> mono = Mono.fromSupplier(() -> Util.faker().country().name())
+		Mono<String> mono = getCountry()
 				.transform(Util.monoLogger("repeatCountry"));
 
 		var repeatedMono = !repeatCount.equals(0) ? mono.repeat(repeatCount)
 				: mono.repeat().take(10); // block Inf loop
 		repeatedMono.subscribe();
+	}
 
+	private static void conditionalRepeat() {
+		getCountry()
+				.repeat()
+				.takeUntil("canada"::equalsIgnoreCase)
+				.subscribe(Util.subscriber("conditionalRepeat"));
+	}
+
+	private static void conditionAsBooleanSupplier() {
+		AtomicInteger counter =  new AtomicInteger(0);
+
+		getCountry()
+				.repeat(() -> counter.getAndIncrement() < 3)
+				.subscribe(Util.subscriber("conditionAsBooleanSupplier"));
+	}
+
+	private static void delayedRepeat() {
+		getCountry()
+				.repeatWhen(flux -> flux.delayElements(Duration.ofSeconds(2)))
+				.subscribe(Util.subscriber("delayedRepeat"));
+
+		Util.sleepSeconds(6);
+	}
+
+	private static void delayedRepeatWithTake() {
+		getCountry()
+				.repeatWhen(flux -> flux.delayElements(Duration.ofSeconds(2)).take(2))
+				.subscribe(Util.subscriber("delayedRepeatWithTake"));
+
+		Util.sleepSeconds(6);
+	}
+
+	private static void repeatWithFlux() {
+		Flux.just(1, 2, 3)
+				.repeat(3)
+				.subscribe(Util.subscriber("repeatWithFlux"));
+	}
+
+	private static Mono<String> getCountry() {
+		return Mono.fromSupplier(() -> Util.faker().country().name());
 	}
 }
