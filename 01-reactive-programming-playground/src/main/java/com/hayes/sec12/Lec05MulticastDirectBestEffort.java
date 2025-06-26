741ffd6d92beb4cdab8d0d8aca1e186c1a5b9e21
Hayes <92khsang@gmail.com>
2025-06-26 15:25:57 +0900

    Lecture 172. Sink Many - Multicast - Direct Best Effort

---
+++ 01-reactive-programming-playground/src/main/java/com/hayes/sec12/Lec05MulticastDirectBestEffort.java
@@ -0,0 +1,55 @@
+package com.hayes.sec12;
+
+import java.time.Duration;
+
+import com.hayes.common.Util;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
+@Slf4j
+public class Lec05MulticastDirectBestEffort {
+
+	public static void main(String[] args) {
+//		System.setProperty("reactor.bufferSize.small", "16");
+//		stage(Sinks.many().multicast().onBackpressureBuffer(16));
+//		stage(Sinks.many().multicast().directBestEffort());
+
+		stageWithMikeBuffer(Sinks.many().multicast().directBestEffort());
+	}
+
+	private static void stage(Sinks.Many<String> sink) {
+		Flux<String> flux = sink.asFlux();
+
+		flux.subscribe(Util.subscriber("Sam"));
+		flux
+				.delayElements(Duration.ofMillis(200))
+				.subscribe(Util.subscriber("Mike"));
+
+		tryEmitNext(sink);
+	}
+
+	private static void stageWithMikeBuffer(Sinks.Many<String> sink) {
+
+		Flux<String> flux = sink.asFlux();
+
+		flux.subscribe(Util.subscriber("Sam"));
+		flux
+				.onBackpressureBuffer()
+				.delayElements(Duration.ofMillis(200))
+				.subscribe(Util.subscriber("Mike"));
+
+		tryEmitNext(sink);
+	}
+
+	private static void tryEmitNext(Sinks.Many<String> sink) {
+		for (int i = 0; i < 32; i++) {
+			Sinks.EmitResult result = sink.tryEmitNext(String.valueOf(i));
+			if (!Sinks.EmitResult.OK.equals(result)) {
+				log.info("Failed because of {} at {}", result, i);
+			}
+		}
+
+		Util.sleepSeconds(8);
+	}
+}
