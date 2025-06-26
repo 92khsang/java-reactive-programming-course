e4a9e5d30f132a25deb1f8e4758b0b3e3d40bea7
Hayes <92khsang@gmail.com>
2025-06-26 14:56:36 +0900

    Lecture 171. Sink Many - Multicast

---
+++ 01-reactive-programming-playground/src/main/java/com/hayes/sec12/Lec04Multicast.java
@@ -0,0 +1,53 @@
+package com.hayes.sec12;
+
+import java.util.function.Consumer;
+
+import com.hayes.common.Util;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
+@Slf4j
+public class Lec04Multicast {
+
+	public static void main(String[] args) {
+		doDemo(sink -> {
+			// Emitting values before subscriber joins
+			sink.tryEmitNext("hi");
+			sink.tryEmitNext("how are you?");
+			sink.tryEmitNext("someone?");
+		}, "tryEmitNext");
+
+
+		Util.printCutoffLIne();
+		multiSub();
+	}
+
+	private static void doDemo(Consumer<Sinks.Many<String>> doSomething, String subName) {
+		Sinks.Many<String> sink = Sinks.many().multicast().onBackpressureBuffer();
+
+		doSomething.accept(sink);
+
+		Flux<String> flux = sink.asFlux();
+		flux.subscribe(Util.subscriber(subName));
+	}
+
+	private static void multiSub() {
+		Sinks.Many<String> sink = Sinks.many().multicast().onBackpressureBuffer();
+
+		for (int i = 1; i <= 3; i++) {
+			Sinks.EmitResult result = sink.tryEmitNext("Multi Before " + i);
+			log.info("[First] EmitResult-{} {}", i, result);
+		}
+
+		Flux<String> flux = sink.asFlux();
+
+		flux.subscribe(Util.subscriber("Multi-1"));
+		flux.subscribe(Util.subscriber("Multi-2"));
+
+		for (int i = 1; i <= 3; i++) {
+			Sinks.EmitResult result = sink.tryEmitNext("Multi After " + i);
+			log.info("[Second] EmitResult-{} {}", i, result);
+		}
+	}
+}
