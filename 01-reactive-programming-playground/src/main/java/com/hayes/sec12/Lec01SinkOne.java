6663e8414422729f16fdece738d66c6f3387a314
Hayes <92khsang@gmail.com>
2025-06-26 13:58:43 +0900

    Lecture 167. Sink One - Emit Failure Handler

--- 01-reactive-programming-playground/src/main/java/com/hayes/sec12/Lec01SinkOne.java
+++ 01-reactive-programming-playground/src/main/java/com/hayes/sec12/Lec01SinkOne.java
@@ -3,19 +3,24 @@ package com.hayes.sec12;
 import java.util.function.Consumer;
 
 import com.hayes.common.Util;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 
+@Slf4j
 public class Lec01SinkOne {
 
 	public static void main(String[] args) {
-		doDemo(sink -> {
-		}, "doNothing");
-		Util.printCutoffLIne();
-		doDemo(sink -> sink.tryEmitValue("Hello"), "tryEmitValue");
-		doDemo(Sinks.Empty::tryEmitEmpty, "tryEmitEmpty");
-		doDemo(sink -> sink.tryEmitError(new RuntimeException("Oops")), "tryEmitError");
-		multiSub();
+//		doDemo(sink -> {
+//		}, "doNothing");
+//		Util.printCutoffLIne();
+//		doDemo(sink -> sink.tryEmitValue("Hello"), "tryEmitValue");
+//		doDemo(Sinks.Empty::tryEmitEmpty, "tryEmitEmpty");
+//		doDemo(sink -> sink.tryEmitError(new RuntimeException("Oops")), "tryEmitError");
+//		multiSub();
+
+		emitValue();
+
 	}
 
 	private static void doDemo(Consumer<Sinks.One<String>> doSomething, String subName) {
@@ -34,13 +39,34 @@ public class Lec01SinkOne {
 		// Create a Sink that can emit one value
 		Sinks.One<String> sink = Sinks.one();
 
-		sink.tryEmitValue("Multi");
+		Sinks.EmitResult result = sink.tryEmitValue("Multi Before");
+		log.info("[First] EmitResult {}", result);
 
 		// Turn the Sink into a Mono
 		Mono<String> mono = sink.asMono();
+
 		// Subscribe to it
 		mono.subscribe(Util.subscriber("Multi-1"));
 		mono.subscribe(Util.subscriber("Multi-2"));
+
+		result = sink.tryEmitValue("Multi After");
+		log.info("[Second] EmitResult {}", result);
+	}
+
+	private static void emitValue() {
+		Sinks.One<String> sink = Sinks.one();
+
+		sink.asMono().subscribe(Util.subscriber("emitValue "));
+
+		sink.emitValue("hi", ((signalType, emitResult) -> {
+			log.info("Hi, Signal: {}, Result: {}", signalType, emitResult);
+			return false;
+		}));
+
+		sink.emitValue("hello", ((signalType, emitResult) -> {
+			log.info("Hello, Signal: {}, Result: {}", signalType, emitResult);
+			return false; // Retry or Not
+		}));
 	}
 
 }
